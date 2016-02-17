769515f0812ff7e6e84168effd78664c36d395cf
Jelmerro <Jelmerro@Jelmerro-PC.home>
2016-02-18 00:07:01 +0100

    Tried fixing, didn't work. Now let's all hope it magically works with tomcat.

--- src/main/java/nl/hu/geoict/rest/GeoService.java
+++ src/main/java/nl/hu/geoict/rest/GeoService.java
@@ -1,5 +1,16 @@
 package nl.hu.geoict.rest;
 
-public class GeoService {
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
+@Path("rest")
+public class GeoService {
+    
+    @GET
+    @Path("/{lat1}/{long1}/{lat2}/{long2}")
+    @Produces({MediaType.TEXT_PLAIN})
+    public Response something(@PathParam("lat1") double lat1, @PathParam("long1") double long1, @PathParam("lat2") double lat2, @PathParam("long2") double long2) {
+        return Response.status(200).entity(DistanceCalc.distance(lat1, long1, lat2, long2)).build();
+    }
 }
