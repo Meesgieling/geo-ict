54392b4a2ee2567fa117531e8b986cf49ae66285
Luc (Windows) <lucvanderzandt@kpnplanet.nl>
2016-02-18 13:32:44 +0100

    Fix path

--- src/main/java/nl/hu/geoict/rest/GeoService.java
+++ src/main/java/nl/hu/geoict/rest/GeoService.java
@@ -4,12 +4,12 @@ import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-@Path("rest")
+@Path("/rest")
 public class GeoService {
     
     @GET
     @Path("/{lat1}/{long1}/{lat2}/{long2}")
-    @Produces({MediaType.TEXT_PLAIN})
+    @Produces({MediaType.APPLICATION_JSON})
     public Response something(@PathParam("lat1") double lat1, @PathParam("long1") double long1, @PathParam("lat2") double lat2, @PathParam("long2") double long2) {
         return Response.status(200).entity(DistanceCalc.distance(lat1, long1, lat2, long2)).build();
     }
