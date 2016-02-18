bd9c8d656b2da385f0b2b49a4ffa1b4100f13e79
Meesgieling <meesgieling@live.nl>
2016-02-18 14:15:12 +0100

    working

--- src/main/java/nl/hu/geoict/rest/DistanceCalc.java
+++ src/main/java/nl/hu/geoict/rest/DistanceCalc.java
@@ -1,15 +1,26 @@
 package nl.hu.geoict.rest;
 
+import javax.xml.bind.annotation.*;
+
+@XmlRootElement
 public class DistanceCalc {
+	public double distance;
 
-    public static void main(String[] args) {
-        System.out.println("test1: " + distance(52.1748705, 4.9743146, 52.0618651, 8.4672746));
-        System.out.println("test2: " + distance(-52.1748705, 4.9743146, 52.0618651, 8.4672746));
-    }
+	@XmlAttribute
+	public String unit = "km";
+	public double lat1;
+	public double lon1;
+	public double lat2;
+	public double lon2;
 
-    public static double distance(double lat1, double lon1, double lat2, double lon2) {
-    double p = Math.PI / 180;
-    double a = 0.5 - Math.cos((lat2 - lat1) * p) / 2 + Math.cos(lat1 * p) * Math.cos(lat2 * p) * (1 - Math.cos((lon2 - lon1) * p)) / 2;
-    return 12742 * Math.asin(Math.sqrt(a));
-    }
+	public void calcDistance(double lat1, double lon1, double lat2, double lon2) {
+		this.lat1 = lat1;
+		this.lon1 = lon1;
+		this.lat2 = lat2;
+		this.lon2 = lon2;
+		double p = Math.PI / 180;
+		double a = 0.5 - Math.cos((lat2 - lat1) * p) / 2 + Math.cos(lat1 * p)
+				* Math.cos(lat2 * p) * (1 - Math.cos((lon2 - lon1) * p)) / 2;
+		distance = 12742 * Math.asin(Math.sqrt(a));
+	}
 }
--- src/main/java/nl/hu/geoict/rest/GeoService.java
+++ src/main/java/nl/hu/geoict/rest/GeoService.java
@@ -4,13 +4,15 @@ import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-@Path("/rest")
+@Path("/distance")
 public class GeoService {
     
     @GET
     @Path("/{lat1}/{long1}/{lat2}/{long2}")
     @Produces({MediaType.APPLICATION_JSON})
     public Response something(@PathParam("lat1") double lat1, @PathParam("long1") double long1, @PathParam("lat2") double lat2, @PathParam("long2") double long2) {
-        return Response.status(200).entity(DistanceCalc.distance(lat1, long1, lat2, long2)).build();
+    	DistanceCalc dc = new DistanceCalc();
+    	dc.calcDistance(lat1, long1, lat2, long2);
+        return Response.status(200).entity(dc).build();
     }
 }
